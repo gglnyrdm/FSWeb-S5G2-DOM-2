54b90d18dd634271f95d036e705424565f2945d5
Gülin <yardimoglugulin@gmail.com>
2023-04-19 00:17:33 +0300

    bazı event listener lar eklendi

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
 import './less/index.less'
 
 // Örnek bir event kullanımı aşağıdadır. Çalıştırmak için comment dışına alın
-// document.querySelector("h1").addEventListener("click",function(e){
-// alert("Bana tıkladın!")
-// });
+document.querySelector("h1").addEventListener("click",function(e){
+alert("Bana tıkladın!")
+});
 
 
 // Kodlar buraya gelecek!
+
+const buttons = document.querySelectorAll(".btn");
+buttons.forEach((button)=>{
+    button.addEventListener("mouseenter",(e)=>{
+        e.target.style.color = "orange";
+    });
+    button.addEventListener("mouseout",(e)=>{
+        e.target.style.color = "white";
+    })
+});
+
+document.getElementsByTagName("a").addEventListener("wheel",(e)=>{
+    e.target.style.color = "purple";
+})
+
+document.load = function(){
+	alert("Sayfa yüklendikten sonra kullanıcıya bildir");
+}
+
+const navFocus = document.querySelectorAll("a");
+navFocus.forEach(a => {
+    a.addEventListener("focus",(e)=>{
+        e.target.color = "red";
+    })
+});
+
+window.addEventListener("resize", function(event) {
+    alert("Tarayıcı boyutu değiştirildi!");
+  });
